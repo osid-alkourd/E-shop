4934fe4a1b4c8346ec21a9881e08057ccab393ca
Osid ALkourd <alkordosid73@gmail.com>
2025-04-16 18:03:24 +0300

    add login and signin components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import{LoginPage, SignupPage} from './routes/Routes'
+import "./App.css"
 const App = () => {
   return(
-    <div>App</div>
+    <div>
+       <BrowserRouter>
+            <Routes>
+              <Route path='/login' element={<LoginPage/>}></Route>
+              <Route path='/sign-up' element={<SignupPage/>}></Route>
+            </Routes>
+       </BrowserRouter>
+    </div>
   )
 } 
 export default App;
---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,5 @@
+import LoginPage from "../pages/Login";
+import SignupPage from "../pages/SignupPage";
+
+
+export {LoginPage,SignupPage}
